0802f83aed653226bc957a574425cb52bad3be64
Muhammad Habban <mhabban2004@gmail.com>
2022-08-04 14:56:42 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+function myFunction() {
+    location.href = "index.html";
+}
+
+function myFunction1() {
+    location.href = "destination-moon.html";
+}
+
+function myFunction2() {
+    location.href = "crew-commander.html";
+}
+
+function myFunction3() {
+    location.href = "technology-vehicle.html";
+}
+
+
+function mooneh() {
+    location.href = "destination-moon.html";
+}
+
+function marseh() {
+    location.href = "destination-mars.html";
+}
+
+function jupitereh() {
+    location.href = "destination-europa.html";
+}
+
+function titaneh() {
+    location.href = "destination-titan.html";
+}
+
+function crew1() {
+    location.href = "crew-commander.html";
+}
+
+function crew2() {
+    location.href = "crew-specialist.html";
+}
+
+function crew3() {
+    location.href = "crew-pilot.html";
+}
+
+function crew4() {
+    location.href = "crew-engineer.html";
+}
+
+function tech1() {
+    location.href = "technology-vehicle.html";
+}
+
+function tech2() {
+    location.href = "technology-spaceport.html";
+}
+
+function tech3() {
+    location.href = "technology-capsule.html";
+}
+
+function ham() {
+    document.getElementById('sideNav').style.width = "250px";
+}
+
+function closingFuntion() {
+    document.getElementById('sideNav').style.width = "0px";
+}
